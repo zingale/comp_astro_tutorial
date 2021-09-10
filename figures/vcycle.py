4bb6adb29b3103e78c60e17f9eba2f29d56ff829
Michael Zingale <michael.zingale@stonybrook.edu>
2021-09-10 10:13:12 -0400

    update figure

--- figures/vcycle.py
+++ figures/vcycle.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 G = nx.MultiDiGraph()
 G.position = {}
-#G.labels = {}
+G.labels = {}
 
 nlevels = 5
 dx = 0.2
@@ -19,11 +19,16 @@ for n in range(nlevels-1):
     print(node)
     G.add_node(node)
     G.position[node] = (n*dx, -n*dy)
+    if n == 0:
+        G.labels[node] = r"$\nabla^2 \phi^h = f^h$"
+    else:
+        G.labels[node] = rf"$\nabla^2 e^{{{2*n}h}} = r^{{{2*n}h}}$"
 
 # bottom
 node = "bottom"
 G.add_node(node)
 G.position[node] = ((nlevels-1)*dx, -(nlevels-1)*dy)
+G.labels[node] = "bottom solve"
 
 # up
 
@@ -32,6 +37,10 @@ for n in range(nlevels-1):
     print(node)
     G.add_node(node)
     G.position[node] = ((nlevels + n)*dx, -(nlevels-2-n)*dy)
+    if n == nlevels-2:
+        G.labels[node] = r"$\nabla^2 \phi^h = f^h$"
+    else:
+        G.labels[node] = rf"$\nabla^2 e^{{{2*(nlevels-2-n)}h}} = r^{{{2*(nlevels-2-n)}h}}$"
 
 
 # now do the edges
@@ -65,8 +74,10 @@ for e in edges:
 nx.draw_networkx_edge_labels(G, G.position, edge_labels=edge_labels,
                              font_size="12", font_color="C0")
 
+nx.draw_networkx_labels(G, G.position, labels=G.labels)
+
 ax.axis("off")
-fig.set_size_inches(10, 10)
+fig.set_size_inches(8, 8)
 
 fig.tight_layout()
-fig.savefig("vcycle.png", dpi=200)
+fig.savefig("vcycle.png", dpi=100)
